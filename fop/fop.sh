c3c692dc6da48bcce22b6103091cd690e66818c1
maestrofjp <maestrofjp@gmail.com>
2010-05-25 02:48:26 +0000

    closes #1

--- fop/fop.sh
+++ fop/fop.sh
@@ -52,6 +52,25 @@ if $cygwin ; then
     CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
 fi
 
+if [ -z "$JAVACMD" ] ; then 
+  if [ -n "$JAVA_HOME"  ] ; then
+    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then 
+      # IBM's JDK on AIX uses strange locations for the executables
+      JAVACMD=$JAVA_HOME/jre/sh/java
+    else
+      JAVACMD=$JAVA_HOME/bin/java
+    fi
+  else
+    JAVACMD=java
+  fi
+fi
+ 
+#if [ ! -x "$JAVACMD" ] ; then
+#  echo "Error: JAVA_HOME is not defined correctly."
+#  echo "  We cannot execute '$JAVACMD'"
+#  exit
+#fi
+
 if [ -n "$CLASSPATH" ] ; then
   LOCALCLASSPATH=$CLASSPATH
 fi
@@ -82,4 +101,5 @@ if $cygwin; then
 fi
 
 java -classpath "$LOCALCLASSPATH" $FOP_OPTS org.apache.fop.apps.Fop "$@"
+
 
